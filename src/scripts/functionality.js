3bad9836b2cb823b8add9b379abe52a09db39fda
sbassalian <solomonbassalian@gmail.com>
2021-04-09 14:17:06 -0400

    pokemon update 1

---
+++ src/scripts/functionality.js
@@ -0,0 +1,277 @@
+import {getPokemon} from './game.js';
+
+let pokemon = await getPokemon();
+
+let player1data = [];
+let player2data = [];
+
+let player2moves= [];
+
+async function getDamage(name) {
+    let moveinfo = []
+    const url3 = `https://pokeapi.co/api/v2/move/${name}/`;
+    await fetch(url3).then(res => res.json()).then(data => {
+        moveinfo.push(data.name);
+        moveinfo.push(data.power);
+        moveinfo.push(data.type.name);
+    })
+
+    return moveinfo;
+
+
+}
+
+
+// class Pokemon{
+//     constructor(name, front, back, hp, type, move1, move2, move3, move4){
+//         this.name = name;
+//         this.front = front;
+//         this.back = back;
+//         this.hp = hp;
+//         this.type = type;
+//         this.move1 = move1;
+//         this.move2 = move2;
+//         this.move3 = move3;
+//         this.move4 = move4;
+//     }
+// }
+
+pokemon.forEach((poke, i) => {
+    let span = document.createElement('span');
+    span.setAttribute("id", `${i}`);
+    
+    document.getElementById('index').appendChild(span);
+    let button = document.createElement('button');
+    button.addEventListener('click', ((e) => initialspawn(e.path[1].id)));
+    button.addEventListener('click', (() => clearMoves()));
+    button.addEventListener('click', ((e) => fetchMoves(e.path[1].id)));
+    button.setAttribute("id", `${poke.name}`);
+    document.getElementById(`${i}`).appendChild(button);
+    let pokepic = document.createElement('img');
+    pokepic.src = poke.front;
+    document.getElementById(`${poke.name}`).appendChild(pokepic);
+})
+
+
+
+function initialspawn(id){
+   let poke = pokemon[id];
+    
+    let randomvar = document.getElementById('fixpokename')
+  
+    randomvar.innerHTML = poke.name;
+
+    player1data = [];
+    player1data.push(poke.name, poke.front, poke.back, poke.hp + 200, poke.types)
+   
+    
+}
+
+
+function clearMoves(){
+    document.getElementById('index3').innerHTML = "";
+}
+
+
+
+function fetchMoves(id){
+    pokemon[id].moves.forEach((move, i) => {
+        let values = Object.values(move);
+        let name = values[0].name;
+        let damage = 0;
+        let span = document.createElement('span');
+        span.setAttribute("id", `${name}`);
+        // span.setAttribute("class", 'moves');
+        document.getElementById('index3').appendChild(span);
+        let button = document.createElement('button');
+        button.setAttribute("id", `${name}1`);
+        button.addEventListener('click', ((e) => spawnmoves(e.path[1].id)));
+        document.getElementById(`${name}`).appendChild(button);
+        getDamage(name).then(data => {
+            damage = data[1];
+            if (!damage){
+                damage = 0
+            }
+        }).then(() => document.getElementById(`${name}1`).innerHTML = `${name}` + ' : ' + ` Power = ${damage}` )
+        ;
+    })
+}
+
+let pokemoves = [];
+
+function spawnmoves(move) {
+    if (pokemoves.length < 3){
+        pokemoves.push(move)
+        player1data.push(move);
+        let span = document.createElement('span');
+        span.innerHTML = move;
+        document.getElementById('poke-info').appendChild(span);
+        
+    }
+    else if (pokemoves.length === 3){
+        pokemoves.push(move)
+        player1data.push(move);
+        let span = document.createElement('span');
+        span.setAttribute("id", 'last-move');
+        span.innerHTML = move;
+        document.getElementById('poke-info').appendChild(span);
+    }
+    else if (pokemoves.length === 4) {
+        pokemoves.pop();
+        pokemoves.push(move);
+        player1data.pop();
+        player1data.push(move);
+        let last = document.getElementById('last-move');
+        last.innerHTML = move;
+        console.log(pokemoves);
+    }
+
+}
+
+function spawnPlayer1(){
+    
+    let player1pic = document.createElement('img');
+    player1pic.src = player1data[2];
+    player1pic.setAttribute("id", 'player1');
+    document.getElementById(`player1box`).appendChild(player1pic);
+    let move1 = document.getElementById('move1');
+    let move2 = document.getElementById('move2');
+    let move3 = document.getElementById('move3');
+    let move4 = document.getElementById('move4');
+    move1.innerHTML = player1data[5];
+    move2.innerHTML = player1data[6];
+    move3.innerHTML = player1data[7];
+    move4.innerHTML = player1data[8];
+    let as2 = document.getElementById('as2');
+    as2.innerHTML = 'Type : ' + player1data[4];
+
+}
+
+
+
+function spawnPlayer2(){
+    let randompoke = pokemon[Math.floor(Math.random() * pokemon.length)]
+
+    let randommove1a = randompoke.moves[Math.floor(Math.random() * randompoke.moves.length)];
+    let randommove1 = Object.values(randommove1a)[0].name;
+    let randommove2a = randompoke.moves[Math.floor(Math.random() * randompoke.moves.length)];
+    let randommove2 = Object.values(randommove2a)[0].name;
+    let randommove3a = randompoke.moves[Math.floor(Math.random() * randompoke.moves.length)];
+    let randommove3 = Object.values(randommove3a)[0].name;
+    let randommove4a = randompoke.moves[Math.floor(Math.random() * randompoke.moves.length)];
+    let randommove4 = Object.values(randommove4a)[0].name;
+    
+    player2data.push(randompoke.name, randompoke.front, randompoke.back, randompoke.hp + 200, randompoke.types, randommove1, randommove2, randommove3, randommove4 )
+
+    player2moves.push(randommove1, randommove2, randommove3, randommove4)
+
+    let player2pic = document.createElement('img');
+    player2pic.src = randompoke.front;
+    player2pic.setAttribute("id", 'player2');
+    document.getElementById(`player2box`).appendChild(player2pic);
+
+    
+    
+}
+
+function startGame(){
+
+
+    let button = document.getElementById('testingbutton');
+    button.addEventListener('click', spawnPlayer1());
+    button.addEventListener('click', spawnPlayer2());
+
+    console.log("hit me");
+    
+    let span = document.createElement('span');
+    let hpinfo = player1data[3];
+    span.innerHTML = hpinfo;
+    span.setAttribute("id", 'battlehp1');
+    document.getElementById('player-1-going').appendChild(span);
+
+    let span2 = document.createElement('span');
+    let hpinfo2 = player2data[3];
+    span2.innerHTML = hpinfo2;
+    span2.setAttribute("id", 'battlehp2');
+    document.getElementById('player-2-going').appendChild(span2);
+
+
+}
+
+let button100 = document.getElementById('testingbutton');
+button100.addEventListener('click', startGame);
+
+
+
+document.getElementById('move1').addEventListener('click', ()=> attack(player1data[0], player2data[0], getDamage(player1data[5])) )
+
+document.getElementById('move2').addEventListener('click', () => attack(player1data[0], player2data[0], getDamage(player1data[6])))
+
+document.getElementById('move3').addEventListener('click', () => attack(player1data[0], player2data[0], getDamage(player1data[7])))
+
+document.getElementById('move4').addEventListener('click', () => attack(player1data[0], player2data[0], getDamage(player1data[8])))
+
+
+function attack(attacker, defender, move) {
+    let damage = 0;
+
+    move.then(data => {
+        console.log(data);
+        console.log(data[1])
+        console.log(data[0])
+        if (!!data[1]){
+            damage = data[1]
+        }
+    });
+
+    
+    move.then(data => document.getElementById('desc').innerHTML = attacker + ' used ' + data[0] + ' on ' + defender + '!' ).then(() => document.getElementById('battlehp2').innerHTML -= damage ).then(()=> {
+        if (document.getElementById('battlehp2').innerHTML <= 0){
+            document.getElementById('desc').innerHTML = attacker + ' Won! ' + ' Please refresh page to start a new battle!'
+        }
+        else{
+            setTimeout(enemyAttack(defender, attacker), 50000)
+        }
+    })
+
+    
+}
+
+
+function enemyAttack(attacker, defender){
+
+    let battlemove = player2moves[Math.floor(Math.random() * player2moves.length)]
+
+    let damage = 0;
+
+    getDamage(battlemove).then(data => {
+
+        if (!!data[1]) {
+            damage = data[1]
+        }
+    });
+
+    getDamage(battlemove).then(data => document.getElementById('desc').innerHTML = attacker + ' used ' + battlemove + ' on ' + defender + '!').then(() => document.getElementById('battlehp1').innerHTML -= damage ).then(()=> {
+        if (document.getElementById('battlehp1').innerHTML <= 0) {
+            document.getElementById('desc').innerHTML = attacker + ' Won! ' + ' Please refresh page to start a new battle!'
+        }
+    })
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/scripts/game.js
@@ -0,0 +1,71 @@
+
+
+const pokemon = [];
+
+export async function getPokemon(){
+    for (let i = 1; i < 200; i++) {
+   
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`
+
+        await fetch(url).then(res => res.json()).then
+            (data => {
+                let obj = {};
+                obj['name'] = data.name;
+                obj['front'] = data.sprites.front_default;
+                obj['back'] = data.sprites.back_default;
+                obj['moves'] = [];
+                data.moves.forEach((move) => {
+                    let obj2 = {};
+
+                    obj2[`${move.move.name}`] = (move.move);
+                    obj['moves'].push(obj2);
+
+                })
+
+                obj['types'] = [];
+
+                data.types.forEach((type) => {
+
+                    obj.types.push(`${type.type.name}`)
+
+                })
+                
+                obj['hp'] = data.stats[0].base_stat;
+                
+                
+                // return obj;
+                pokemon.push(obj)
+            }
+        )
+        // .then((obj) => pokemon.push(obj))
+        // .then(() => {if (pokemon.length === 9){return(pokemon)}});
+    }
+    return pokemon;
+}
+
+// const getMoves = () => {
+//     const url2 = "https://pokeapi.co/api/v2/move/5/";
+//     fetch(url2).then(res => res.json()).then(data => console.log(data));
+// }
+
+// my set up will make it so its first name, then power, then type of move
+
+// export async function getDamage(){
+//     let moveinfo = []
+//     const url3 = "https://pokeapi.co/api/v2/move/89/";
+//     await fetch(url3).then(res => res.json()).then(data => {
+//         moveinfo.push(data.name);
+//         moveinfo.push(data.power);
+//         moveinfo.push(data.type.name);
+//     })
+//     // console.log(moveinfo);
+// }
+
+
+// getPokemon();
+// getMoves();
+// getDamage();
+
+
+// module.exports = {getPokemon}
+
